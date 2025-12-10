e86bf9c7caef64b70efa686fced6711b2c1d86c1
Sophie <soph1155@stud.kea.dk>
2025-12-10 20:59:08 +0100

    continue structure of project

--- js/products.js
+++ js/products.js
@@ -1,3 +1,28 @@
+fetch("https://fakestoreapi.com/products")
+  .then(res => res.json())
+  .then(data => {
+    console.log("Products loaded:", data);
+  })
+  .catch(err => console.error("API fetch error:", err));
+
+import { BASE_URL } from './info.js';
+
+document.addEventListener("DOMContentLoaded", async () => {
+    const container = document.querySelector('#products-container');
+
+    const res = await fetch(`${BASE_URL}/products`);
+    const products = await res.json();
+
+    container.innerHTML = products.map(product => `
+        <article class="product-card">
+            <img src="${product.image}" alt="${product.title}">
+            <h3>${product.title}</h3>
+            <p>${product.category}</p>
+            <strong>${product.price} $</strong>
+        </article>
+    `).join('');
+});
+
 // URL to fetch all products
 const productsURL = 'https://fakestoreapi.com/products';
 
