37d41d0180a0dacb3ab1f85107a47c43ed8b8cba
Sophie <soph1155@stud.kea.dk>
2025-12-11 11:58:08 +0100

    adding logout

--- js/logout.js
+++ js/logout.js
@@ -1,18 +1,12 @@
-// import { SESSION_STORAGE_USER_EMAIL } from './info.js';
+import { SESSION_STORAGE_USER_EMAIL } from './info.js';
 
-// const logoutBtn = document.querySelector('#btnLogout') || document.querySelector('#logout');
+const logoutBtn = document.querySelector('#btnLogout') || document.querySelector('#logout');
 
-// if (logoutBtn) {
-//     logoutBtn.addEventListener('click', () => {
-//         sessionStorage.removeItem(SESSION_STORAGE_USER_EMAIL);
+if (logoutBtn) {
+    logoutBtn.addEventListener('click', () => {
+        sessionStorage.removeItem(SESSION_STORAGE_USER_EMAIL);
 
-//         location.href = 'index.html';
-//     });
-// }
+        location.href = 'index.html';
+    });
+}
 
-// js/logout.js
-document.querySelector('#logoutBtn')?.addEventListener('click', (e) => {
-  localStorage.removeItem('userEmail');
-  // lokal redirect:
-  window.location.href = 'index.html';
-});
---
+++ js/nav.js
@@ -0,0 +1,49 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const userEmail = localStorage.getItem('userEmail');
+
+//     const loginEl = document.querySelector('#login');
+//     const signupEl = document.querySelector('#signup');
+//     const logoutEl = document.querySelector('#logout');
+
+//     if (userEmail) {
+       
+//         loginEl.classList.add('hidden');
+//         signupEl.classList.add('hidden');
+//         logoutEl.classList.remove('hidden');
+//     } else {
+     
+//         loginEl.classList.remove('hidden');
+//         signupEl.classList.remove('hidden');
+//         logoutEl.classList.add('hidden');
+//     }
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const userEmail = localStorage.getItem('userEmail');
+
+    const loginEl = document.querySelector('#login');
+    const signupEl = document.querySelector('#signup');
+    const logoutEl = document.querySelector('#logout');
+    const userEl = document.querySelector('#user');
+    const userEmailSpan = document.querySelector('#userEmail');
+
+    if (userEmail) {
+        // User is logged in
+        loginEl.classList.add('hidden');
+        signupEl.classList.add('hidden');
+        logoutEl.classList.remove('hidden');
+        if (userEl && userEmailSpan) {
+            userEl.classList.remove('hidden');
+            userEmailSpan.textContent = userEmail;
+        }
+    } else {
+        // User is not logged in
+        loginEl.classList.remove('hidden');
+        signupEl.classList.remove('hidden');
+        logoutEl.classList.add('hidden');
+        if (userEl) {
+            userEl.classList.add('hidden');
+            if (userEmailSpan) userEmailSpan.textContent = '';
+        }
+    }
+});
